e71a58f3b1eb7695865451302f5592779e1f75d7
Cédric Bois <cedric.bois@uca.fr>
2021-02-25 21:24:24 +0100

    add react photo gallery + fix hash router

--- assets/js/components/AppRouter.js
+++ assets/js/components/AppRouter.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+
+import Fanfarons from './pages/Fanfarons'
 import Home from './pages/Home'
 
 const AppRouter = () => {
   return (
     <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+      <Route exact path="/" component={Home} />
+      <Route path="/fanfarons" component={Fanfarons} />
     </Switch>
   )
 }
--- assets/js/components/MenuBar.js
+++ assets/js/components/MenuBar.js
@@ -6,37 +6,37 @@ import { Link } from 'react-router-dom'
 const MenuBar = () => {
   return (
     <header>
-      <Link to="/#/" className="header-img">
+      <Link to="/" className="header-img">
         <img src={fanfareBandeau} />
       </Link>
       <nav>
         <ul>
           <li>
-            <Link to="/#/">Fanfare</Link>
+            <Link to="/">Fanfare</Link>
           </li>
           <li>
-            <Link to="/#/fanfarons">Fanfarons</Link>
+            <Link to="/fanfarons">Fanfarons</Link>
           </li>
           <li>
             <span>Spectacle</span>
             <ul>
               <li className="category">Spectacles en cours</li>
               <li>
-                <Link to="/#/fanfaronnades">Fanfaronnades</Link>
+                <Link to="/fanfaronnades">Fanfaronnades</Link>
               </li>
               <li>
-                <Link to="/#/un-grain-dans-rouages">
+                <Link to="/un-grain-dans-rouages">
                   Un grain dans les rouages
                 </Link>
               </li>
               <li className="category">Anciens spectacles</li>
               <li>
-                <Link to="/#/fabuleuse-histoire">
+                <Link to="/fabuleuse-histoire">
                   Une fabuleuse histoire ordinaire
                 </Link>
               </li>
               <li>
-                <Link to="/#/dans-la-rue">Dans la rue</Link>
+                <Link to="/dans-la-rue">Dans la rue</Link>
               </li>
             </ul>
           </li>
@@ -44,21 +44,21 @@ const MenuBar = () => {
             <span>Media</span>
             <ul>
               <li>
-                <Link to="/#/videos">Vidéos</Link>
+                <Link to="/videos">Vidéos</Link>
               </li>
               <li>
-                <Link to="/#/photos">Photos</Link>
+                <Link to="/photos">Photos</Link>
               </li>
             </ul>
           </li>
           <li>
-            <Link to="/#/agenda">Agenda</Link>
+            <Link to="/agenda">Agenda</Link>
           </li>
           <li>
-            <Link to="/#/presse">Presse</Link>
+            <Link to="/presse">Presse</Link>
           </li>
           <li>
-            <Link to="/#/contact">Contact</Link>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
