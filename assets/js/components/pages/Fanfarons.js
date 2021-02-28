b35603e8bada39255ed7f3587887adea08d3c49c
Cédric Bois <cedric.bois@uca.fr>
2021-02-28 19:17:32 +0100

    add react images in Fanfarons component

--- assets/js/components/pages/Fanfarons.js
+++ assets/js/components/pages/Fanfarons.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Gallery from 'react-photo-gallery'
+import Carousel, { Modal, ModalGateway } from 'react-images'
 
 function importAll(r) {
   return r.keys().map(r)
@@ -10,12 +11,45 @@ const images = importAll(
 )
 
 const Fanfarons = () => {
-  const photos = images.map((image) => ({ src: image, width: 2, height: 3 }))
+  const indexesToExpand = []
+
+  const [currentImage, setCurrentImage] = useState(0)
+  const [viewerIsOpen, setViewerIsOpen] = useState(false)
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index)
+    setViewerIsOpen(true)
+  }, [])
+
+  const closeLightbox = () => {
+    setCurrentImage(0)
+    setViewerIsOpen(false)
+  }
+
+  const photos = images.map((image) => ({
+    src: image,
+    width: 2,
+    height: 3
+  }))
 
   return (
     <div>
       <h1>Les fanfarons</h1>
-      <Gallery photos={photos} />
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   )
 }
